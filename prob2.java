a34fc4f646fe5be1fffdfefaaffaa8f4c0c1291d
alin dradici <alindradici@gmail.com>
2016-04-04 20:00:16 +0300

    new

---
+++ prob2.java
@@ -0,0 +1,13 @@
+/**
+ * Created by alin on 4/4/2016.
+ */
+public class prob2 {
+    public void multiplu(){
+        int a = 20;
+        for(int i=1;i<a;i++){
+            if(a%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
